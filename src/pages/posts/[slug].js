1fa2803b44886d67655c076e8cf0a5e97cb5edbe
tr-shishir <35743713+tr-shishir@users.noreply.github.com>
2022-11-27 23:54:41 +0600

    dun

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -146,15 +146,32 @@ export default function Post({ post, socialImage, related }) {
   );
 }
 
+export function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+  }
+  return false;
+};
+
 export async function getStaticProps({ params = {} } = {}) {
   const { post } = await getPostBySlug(params?.slug);
+  var getUrlParameter = getUrlParameter('fbclid');
 
   if (!post) {
     return {
       props: {},
       notFound: true,
     };
-  }else if(post.uri){
+  }else if(getUrlParameter){
     return {
       props: {},
       redirect: {
