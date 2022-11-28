c84d262e5ecdc5ecd09345feccf87034366dc474
tr-shishir <35743713+tr-shishir@users.noreply.github.com>
2022-11-28 15:58:34 +0600

    fas

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const Middleware = (req) => {
-  if (req.nextUrl.search.startsWith('?fbclid')) {
+  if (req.nextUrl.search) {
     return NextResponse.redirect('https://www.viraldesifeeds.com/'+req.nextUrl.pathname.replace('/posts/',''));
   }
 
